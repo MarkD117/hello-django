ec20e6c7ca258cbfab250ba4c98fdef18437dda8
Mark Dunne <Markdunne117@gmail.com>
2023-10-25 19:15:30 +0000

    Create models and migrate

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    # null=False prevents the field from being blank programmatically
+    # blank=False prevents the field from accepting if left blank
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    # Changing the default string name of our created items to the name set
+    def __str__(self):
+        return self.name
