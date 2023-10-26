f1c722e8ad4c61c59d29545ddc1883c9fde0db45
Mark Dunne <Markdunne117@gmail.com>
2023-10-26 20:56:42 +0000

    Add feature to update items

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Item
 from .forms import ItemForm
 
@@ -29,3 +29,23 @@ def add_item(request):
         'form': form
     }
     return render(request, "todo/add_item.html", context)
+
+
+# item_id parameter points to the item od attached to the edit button
+def edit_item(request, item_id):
+    # we want to get an instance of the item model. With an ID
+    # equal to the item ID that was passed into the view via the URL.
+    item = get_object_or_404(Item, id=item_id)
+    if request.method == 'POST':
+        # give form specific item instance we want to update
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+    # pass instance item to form to pre-populate it with the current items
+    # details. This fills the form with the information above from the database
+    form = ItemForm(instance=item)
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/edit_item.html', context)
