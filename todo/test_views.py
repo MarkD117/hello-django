7f5039f88c474b1be3322d42db6033b80b29fca0
Mark Dunne <Markdunne117@gmail.com>
2023-10-29 17:04:19 +0000

    Add comments to test_views.py

--- todo/test_views.py
+++ todo/test_views.py
@@ -18,24 +18,34 @@ class TestViews(TestCase):
         # handling CRUD functionality by creating an item
         item = Item.objects.create(name='Test Todo Item')
         # we can access the created items id through an f string
+        # using a get request to edit the item
         response = self.client.get(f'/edit/{item.id}')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'todo/edit_item.html')
 
     def test_can_add_item(self):
+        # posting a name to the /add url to create an item
         response = self.client.post('/add', {'name': 'Test Added Item'})
+        # if item added successfully, it should redirect to home
         self.assertRedirects(response, '/')
 
     def test_can_delete_item_page(self):
         item = Item.objects.create(name='Test Todo Item')
+        # using get request to delete the item
         response = self.client.get(f'/delete/{item.id}')
         self.assertRedirects(response, '/')
+        # checking if item is deleted by filtering by id
         existing_items = Item.objects.filter(id=item.id)
+        # filter result should be 0 as no item with that id exists
         self.assertEqual(len(existing_items), 0)
 
     def test_can_toggle_item_page(self):
+        # create item with done status of True
         item = Item.objects.create(name='Test Todo Item', done=True)
+        # call toggle request on created item
         response = self.client.get(f'/toggle/{item.id}')
         self.assertRedirects(response, '/')
+        # get the id of the created item
         updated_item = Item.objects.get(id=item.id)
+        # check if done status changed to False
         self.assertFalse(updated_item.done)
